fbbe99f099729ac4a875ccb80c0cd24d52928472
graphistopheles <elgraphistatest@gmail.com>
2026-07-09 11:03:36 -0400

    cambios footer

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -302,7 +302,7 @@ export const footer = {
       links: [
         { label: 'Ayuda', href: '/precios/#faq' },
         { label: 'Postular', href: 'https://wa.link/e0k8uq' },
-        { label: 'Contacto', href: 'mailto:contacto@movercar.pro' },
+        { label: 'Contacto', href: 'https://wa.me/56957918247' },
       ],
     },
   ],
